edb2fb556e8f98cc4320d34c74e7eaa9f421896a
Rahul-M01 <root@DESKTOP-BUO1M60.localdomain>
2025-01-18 20:16:45 +0000

    Initial commit

---
+++ drishyam/src/main/java/com/site/drishyam/DrishyamApplication.java
@@ -0,0 +1,13 @@
+package com.site.drishyam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrishyamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrishyamApplication.class, args);
+	}
+
+}
---
+++ drishyam/src/test/java/com/site/drishyam/DrishyamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.site.drishyam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrishyamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
